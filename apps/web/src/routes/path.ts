33d93c8657fdb39db12bbe4df66cfa528d4ee294
한서정 / Seojung Han <sjhan0814@gmail.com>
2026-02-20 21:19:17 +0900

    feat: update path constants for ranking, reviews, categories, and my page

--- apps/web/src/routes/path.ts
+++ apps/web/src/routes/path.ts
@@ -3,4 +3,19 @@ export const PATH = {
   LOGIN: {
     BASE: '/login',
   },
+  RANKING: {
+    BASE: '/ranking',
+    PRODUCT: '/products',
+    CREATOR_CREDIBILITY: '/creator-credibility',
+  },
+  REVIEWS: {
+    BASE: '/reviews',
+  },
+  CATEGORIES: {
+    BASE: '/categories',
+  },
+  MY_PAGE: {
+    BASE: '/my-page',
+    LIKED_PRODUCTS: '/liked-products',
+  },
 };
