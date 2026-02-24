f74d8039b0d7b661bd5787c949c5f352486abf37
한서정 / Seojung Han <sjhan0814@gmail.com>
2026-02-24 14:40:08 +0900

    feat: 베너

--- apps/web/src/pages/home/HomePage.tsx
+++ apps/web/src/pages/home/HomePage.tsx
@@ -3,8 +3,11 @@ import { RecommendationSection } from '@/features/home';
 
 export const HomePage = () => {
   return (
-    <div className="inline-flex w-full flex-col items-center gap-17">
+    <div className="inline-flex w-full flex-col items-center gap-17 pt-[4.875rem]">
       <RecommendationSection />
+      <div className="flex h-[7.3125rem] w-full items-center justify-center gap-2.5 p-2.5 text-black [background:var(--grey-03,#E4E2DF)]">
+        베너 임시
+      </div>
     </div>
   );
 };
