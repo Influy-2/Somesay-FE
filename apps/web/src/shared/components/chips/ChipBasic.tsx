51f5dba32ee0c1b792aca5b1b47520a7f33e07ee
한서정 / Seojung Han <sjhan0814@gmail.com>
2026-03-19 13:28:28 +0900

    refactor: refactor ChipBasic and ChipLarge components to use utility classnames for styling

--- apps/web/src/shared/components/chips/ChipBasic.tsx
+++ apps/web/src/shared/components/chips/ChipBasic.tsx
@@ -1,4 +1,5 @@
 // Chip/기본, Chip/홈프로필용, Chip/color
+import cn from '@/utils/cn';
 export const ChipBasic = ({
   label,
   bgColor = 'white',
@@ -10,7 +11,11 @@ export const ChipBasic = ({
 }) => {
   return (
     <div
-      className={`bg-${bgColor} text-${textColor} inline-flex items-center justify-center gap-2.5 rounded-[1.25rem] px-2 py-[.1875rem] text-center text-xs leading-[140%] font-medium tracking-[-0.0008rem]`}
+      // className={`bg-${bgColor} text-${textColor} inline-flex items-center justify-center gap-2.5 rounded-[1.25rem] px-2 py-[.1875rem] text-center text-xs leading-[140%] font-medium tracking-[-0.0008rem]`}
+      className={cn(
+        `bg-${bgColor} text-${textColor}`,
+        'inline-flex items-center justify-center gap-2.5 rounded-[1.25rem] px-2 py-[.1875rem] text-center text-xs leading-[140%] font-medium tracking-[-0.0008rem]'
+      )}
     >
       {label}
     </div>
--- apps/web/src/shared/components/chips/ChipLarge.tsx
+++ apps/web/src/shared/components/chips/ChipLarge.tsx
@@ -1,6 +1,21 @@
-export const ChipLarge = ({ label }: { label: string }) => {
+import cn from '@/utils/cn';
+
+export const ChipLarge = ({
+  label,
+  bgColor = 'grey05',
+  textColor = 'grey08',
+}: {
+  label: string;
+  bgColor?: string;
+  textColor?: string;
+}) => {
   return (
-    <div className="bg-grey05 text-grey08 body2-sb inline-flex items-center justify-center gap-2.5 rounded-[1.25rem] px-2.5 py-1 whitespace-nowrap">
+    <div
+      className={cn(
+        `bg-${bgColor} text-${textColor}`,
+        'body2-sb inline-flex items-center justify-center gap-2.5 rounded-[1.25rem] px-2.5 py-1 whitespace-nowrap'
+      )}
+    >
       {label}
     </div>
   );
