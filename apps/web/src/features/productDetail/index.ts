646e647dda08eea625cd37f60e233787b7ac8b36
네넨이 <dlxowns950205@gmail.com>
2026-04-10 02:46:09 +0900

    chore: productdetail index 수정

--- apps/web/src/features/productDetail/index.ts
+++ apps/web/src/features/productDetail/index.ts
@@ -1,9 +1,6 @@
 export { ProductHero } from './components/ProductHero';
-export { CreatorReviewSummary } from './components/CreatorReviewSummary';
-export { Comments } from './components/reviewRanking/Comments';
+export { CreatorReviewSummarySection } from './components/CreatorReviewSummarySection';
 export { ReviewRankingList } from './components/reviewRanking/ReviewRankingList';
-export { ReviewRankingItem } from './components/reviewRanking/ReviewRankingItem';
-export { SimilarProductCard } from './components/similarProducts/SimilarProductCard';
 export { SimilarProductsList } from './components/similarProducts/SimilarProductsList';
 
 export * from './components/mockData';
