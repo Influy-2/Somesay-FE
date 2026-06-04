9c533fcdd3395e6678bf5cf6e3e74057f157cbd9
한서정 / Seojung Han <sjhan0814@gmail.com>
2026-06-04 16:41:56 +0900

    feat: 페이지 헤더

---
+++ apps/web/src/features/home/components/productRecommendation/ProductRecommendationHeader.tsx
@@ -0,0 +1,29 @@
+import { SomesayIcon } from '@/shared/icons';
+export const ProductRecommendationHeader = ({
+  count,
+  keywords,
+}: {
+  count: number;
+  keywords: string[];
+}) => {
+  return (
+    <section className="flex w-full flex-col items-center gap-3 px-4">
+      <h3 className="text-center text-black">
+        <span className="flex items-center justify-center">
+          <SomesayIcon />
+          <span className="headline4">는</span>
+        </span>
+        <span className="headline4 block">{count}개의 제품을 추천드려요</span>
+      </h3>
+
+      <div className="body2-m text-grey08 flex flex-wrap items-center justify-center gap-x-2 gap-y-1">
+        {keywords.map((keyword) => (
+          <span key={keyword}>
+            <span className="text-grey06">#</span>
+            {keyword}
+          </span>
+        ))}
+      </div>
+    </section>
+  );
+};
