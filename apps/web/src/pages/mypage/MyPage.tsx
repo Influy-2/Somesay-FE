7b3f1d6848c5780eefc74304a8962995e62eac30
네넨이 <dlxowns950205@gmail.com>
2026-07-18 07:38:33 +0900

    design: 마이페이지 피그마 디자인 수정

--- apps/web/src/pages/mypage/MyPage.tsx
+++ apps/web/src/pages/mypage/MyPage.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router';
+import { useState } from 'react';
 import { PATH } from '@/routes/path';
-import { MainArrowIcon } from '@/shared/icons';
+import { MainArrow20Icon } from '@/shared/icons';
 import { InfoRow } from '@/features/myPage/components/InfoRow';
 import { InfoSection } from '@/features/myPage/components/InfoSection';
 import { MyPageDashboard } from '@/features/myPage/components/MyPageDashboard';
 import { RecommendedCreators } from '@/features/myPage/components/RecommendedCreators';
-import { PageHeader, ChipBasic } from '@/shared/components';
+import { PageHeader, ChipBasic, Tooltip } from '@/shared/components';
 
 const MOCK_USER = {
   nickname: '닉네임',
@@ -21,13 +22,15 @@ const MOCK_USER = {
 };
 
 export const MyPage = () => {
+  const [showTooltip, setShowTooltip] = useState(true);
+
   return (
     <div className="mt-13.5 flex flex-col">
       <PageHeader title="마이페이지" />
 
       <div className="flex flex-col px-4">
         {/* 프로필 */}
-        <div className="flex items-start py-8">
+        <div className="mb-1 flex items-start py-5">
           <div className="bg-grey02 size-15 shrink-0 overflow-hidden rounded-full" />
           <div className="ml-3 flex flex-1 flex-col gap-2">
             <span className="text-[1.25rem] leading-[150%] font-semibold">
@@ -42,15 +45,32 @@ export const MyPage = () => {
             to={`${PATH.MY_PAGE.BASE}/${PATH.MY_PAGE.ACCOUNT.BASE}`}
             aria-label="계정 설정으로 이동"
           >
-            <MainArrowIcon className="text-grey06" />
+            <MainArrow20Icon />
           </Link>
         </div>
 
         <MyPageDashboard {...MOCK_USER.stats} />
+      </div>
+      <div className="mt-7" />
+
+      <div className="bg-grey01 h-1.75" />
+      <div className="mt-7" />
+
+      <div className="relative px-4">
+        {showTooltip && (
+          <Tooltip
+            label="리뷰를 많이 평가할 수록 추천이 정확해져요!"
+            isVisible={showTooltip}
+            variant="withClose"
+            onClose={() => setShowTooltip(false)}
+            className="-top-12 left-4"
+            arrowPosition="top"
+            arrowClassName="left-6"
+          />
+        )}
         <RecommendedCreators />
       </div>
-
-      <div className="bg-grey01 h-2" />
+      <div className="bg-grey01 h-1.75" />
 
       <div className="divide-grey03 flex flex-col divide-y px-4 pb-10">
         <InfoSection title="나의 활동">
