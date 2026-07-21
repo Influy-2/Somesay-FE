f56dc972ea1e04956a0107b9256f88f9a3d8d54c
네넨이 <dlxowns950205@gmail.com>
2026-07-22 07:43:52 +0900

    design: 상품 히어로 및 브랜드 프로필 수정

--- apps/web/src/features/productDetail/components/ProductHero.tsx
+++ apps/web/src/features/productDetail/components/ProductHero.tsx
@@ -8,12 +8,16 @@ interface ProductHeroProps extends ProductDetailType {
 export const ProductHero = ({
   productImageUrl,
   brandName,
-  brandImageUrl,
+  brandId,
+  brandLogoUrl,
   productName,
   price,
   volume,
   isHearted,
   onLikeClick,
+  collabChannelName,
+  collabProfileImgUrl,
+  productNotes,
 }: ProductHeroProps) => {
   return (
     <section
@@ -29,19 +33,45 @@ export const ProductHero = ({
           />
         )}
       </div>
-      <BrandProfile brandName={brandName} brandImageUrl={brandImageUrl} />
-
-      <div className="px-4 py-5">
+      <BrandProfile
+        brandName={brandName}
+        brandId={brandId}
+        brandImageUrl={brandLogoUrl}
+      />
+      <div className="flex flex-col gap-3.5 px-4 py-5">
         <h2 className="subhead-sb">{productName}</h2>
+        {productNotes && <p className="body2-m text-grey06">{productNotes}</p>}
+        {collabChannelName && (
+          <div className="flex items-center gap-2 px-4">
+            {collabProfileImgUrl && (
+              <div className="bg-grey02 size-5 overflow-hidden rounded-full">
+                <img
+                  src={collabProfileImgUrl}
+                  alt={collabChannelName}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            )}
+            <span className="body2-m">
+              {collabChannelName} 님의 공동 개발 상품
+            </span>
+          </div>
+        )}
         <div className="flex items-center justify-between">
-          <dl className="flex gap-4">
+          <dl className="flex items-center gap-4">
             <div className="flex items-center gap-1">
-              <dt className="body2-b">정가</dt>
-              <dd className="body2-m">{price.toLocaleString()}원</dd>
+              <dt className="body2-b">정가</dt>{' '}
+              <dd className="body2-m">
+                {price
+                  ? price.toLocaleString('ko-KR') + '원'
+                  : '가격 정보 없음'}
+              </dd>
             </div>
             <div className="flex items-center gap-1">
               <dt className="body2-b">용량</dt>
-              <dd className="body2-m">{volume}ml</dd>
+              <dd className="body2-m">
+                {volume ? `${volume}ml` : '용량 정보 없음'}
+              </dd>
             </div>
           </dl>
           <BlackHeartButton isLiked={isHearted} onLikeToggle={onLikeClick} />
